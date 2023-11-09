416d6a747abf6df99a59b2b4f5139a661d046d0f
Radoslav Kiryazov <100125411+radoslavKiryazov@users.noreply.github.com>
2023-11-09 01:48:31 +0100

    forgotten stashed styling line

--- src/components/PokeGrid.jsx
+++ src/components/PokeGrid.jsx
@@ -18,20 +18,20 @@ const PokeGrid = ({ pageData }) => {
         <>
             <div className="flex flex-row">
                 <div className="flex items-center justify-center w-10 ">
-                    {pageData.previous && (<button className="border-2 border-black rounded-full hover:bg-white transition-colors" onClick={onPreviousPage}><img className="w-7 h-7" src={backbutton} alt="backbutton" /></button>)}
+                    {pageData.previous && (<button className="border-2 border-black rounded-full hover:bg-white transition-colors" onClick={onPreviousPage}><img className="w-7 h-7 hover:w-10 hover:h-10 transition-all" src={backbutton} alt="backbutton" /></button>)}
                 </div>
                 <div className="flex flex-col w-[1050px] h-[600px] ">
                 <div className="md:grid grid-cols-4 gap-3 h-fil p-2 w-[1050px] h-[600px]">
                     {pageData.result.map((pokemon) =>
                         (<PokeCard url={pokemon.url} key={pokemon.name} />))}
                 </div>
-                <div className="flex flex-row font-pokemonName px-2 justify-between text-3xl">
-                <h1>{pageNumber}</h1>
-                {pageNumber >= 5 && (<button onClick={jumpToStart}> Jump To Start </button>)}   
+                <div className="flex flex-row font-pokemonName px-2 justify-between text-3xl" key={pageNumber}>
+                <h1 className="animate-fade-right">{pageNumber}</h1>
+                {pageNumber >= 5 && (<button className="animate-fade-left hover:text-4xl transition-all" onClick={jumpToStart}> Jump To Start </button>)}   
                 </div> 
                 </div>
                 <div className="flex items-center justify-center w-10 ">
-                    {pageData.next && (<button className="border-2 border-black rounded-full hover:bg-white transition-colors" onClick={onNextPage}><img className="w-7 h-7" src={forwardbutton} alt="forwardbutton" /></button>)}
+                    {pageData.next && (<button className="border-2 border-black rounded-full hover:bg-white transition-colors" onClick={onNextPage}><img className="w-7 h-7 hover:w-10 hover:h-10 transition-all" src={forwardbutton} alt="forwardbutton" /></button>)}
                 </div>
             </div>
         </>
